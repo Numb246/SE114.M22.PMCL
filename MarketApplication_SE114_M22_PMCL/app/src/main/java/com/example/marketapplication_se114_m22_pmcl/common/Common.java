4086143966a56259c464d4e8850a1ad2ce63a7a2
VietQuoc29 <20520725@gm.uit.edu.vn>
2022-05-07 00:16:14 +0700

    convertCodeToStatus

--- MarketApplication_SE114_M22_PMCL/app/src/main/java/com/example/marketapplication_se114_m22_pmcl/common/Common.java
+++ MarketApplication_SE114_M22_PMCL/app/src/main/java/com/example/marketapplication_se114_m22_pmcl/common/Common.java
@@ -4,4 +4,12 @@ import com.example.marketapplication_se114_m22_pmcl.Model.User;
 
 public class Common {
     public static User currentUser;
+
+    public static String convertCodeToStatus(String code){
+        if(code.equals(0)){
+            return "Placed";
+        } else if (code.equals(1)){
+            return "On my way";
+        } else return "Shipped";
+    }
 }
