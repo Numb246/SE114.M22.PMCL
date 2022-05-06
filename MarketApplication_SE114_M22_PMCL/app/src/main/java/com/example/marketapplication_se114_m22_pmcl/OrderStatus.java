1ce541ae57fdd5ceaa5672ac9260f05883fee170
VietQuoc29 <20520725@gm.uit.edu.vn>
2022-05-06 17:45:39 +0700

    Add OrderStatus Activity

---
+++ MarketApplication_SE114_M22_PMCL/app/src/main/java/com/example/marketapplication_se114_m22_pmcl/OrderStatus.java
@@ -0,0 +1,14 @@
+package com.example.marketapplication_se114_m22_pmcl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_status);
+    }
+}
